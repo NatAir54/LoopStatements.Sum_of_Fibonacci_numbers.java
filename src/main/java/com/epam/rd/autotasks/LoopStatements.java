adfc3b3da7fa8a4dc6c0c693e6f95e0136d42e6b
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-03 23:12:02 +0300

    Fix LoopStatements

--- src/main/java/com/epam/rd/autotasks/LoopStatements.java
+++ src/main/java/com/epam/rd/autotasks/LoopStatements.java
@@ -1,20 +1,21 @@
 package com.epam.rd.autotasks;
-class LoopStatements {
-    public static int task3(int n) throws IllegalArgumentException {
-        if (n>=3) {
-            int[] numbers = new int[n];
-            numbers[0] = 0;
-            numbers[1] = 1;
-            numbers[2] = 1;
-            int sum = 1;
-            for (int i = 2; i < numbers.length; i++) {
-                numbers[i] = numbers[i - 1] + numbers[i - 2];
-                sum = sum + numbers[i];}
-            return sum;
+    class LoopStatements {
+        public static int task3(int n) {
+            if (n<=0) {
+                throw new IllegalArgumentException();}
+            else if (n==1) return 0;
+            else if (n==2) return 1;
+            else if (n==3) return 2;
+            else {
+                int[] numbers = new int[n];
+                numbers[0] = 0;
+                numbers[1] = 1;
+                int sum = 1;
+                for (int i = 2; i < numbers.length; i++) {
+                    numbers[i] = numbers[i - 1] + numbers[i - 2];
+                    sum = sum + numbers[i];}
+                return sum;
+            }
         }
-        else if (n==1) return 0;
-        else if (n==2) return 1;
-        else {
-            throw new IllegalArgumentException();}
     }
-}
+
